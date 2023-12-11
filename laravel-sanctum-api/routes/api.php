00cac6ef09060a740021a0c108dcac3cb269e736
AromalShaji <aromalpshaji10@gmail.com>
2023-12-11 17:10:20 +0530

    Course completed

--- laravel-sanctum-api/routes/api.php
+++ laravel-sanctum-api/routes/api.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::resource('products', ProductController::class);
-
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
